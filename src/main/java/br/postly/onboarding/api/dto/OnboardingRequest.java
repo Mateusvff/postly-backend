49e3c6c6d8062331cacf9ca02e63d99c0ed14cfa
Mateus Franco <mateusfrancovinicius@gmail.com>
2025-11-08 00:40:22 -0300

    refactor: replace @Max with @Size annotations in OnboardingRequest for clarity and consistency

--- src/main/java/br/postly/onboarding/api/dto/OnboardingRequest.java
+++ src/main/java/br/postly/onboarding/api/dto/OnboardingRequest.java
@@ -2,21 +2,19 @@ package br.postly.onboarding.api.dto;
 
 import br.postly.onboarding.domain.enums.MainGoal;
 import br.postly.onboarding.domain.enums.OnboardingStatus;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.Set;
 
 public record OnboardingRequest(
-
-        @NotBlank @Max(30) String igUsername,
-        @NotBlank @Max(50) String niche,
+        @NotBlank @Size(max = 30) String igUsername,
+        @NotBlank @Size(max = 50) String niche,
         @NotNull MainGoal mainGoal,
         @NotNull Integer postPerWeek,
         @NotNull OnboardingStatus status,
         @NotNull Set<String> igReferences,
-        @Max(255) String comments
-
+        @Size(max = 255) String comments
 ) {
 }
