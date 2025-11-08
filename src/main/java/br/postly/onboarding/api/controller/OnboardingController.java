9345810367d04bb4cdc9110fa314dbac021beecc
Mateus Franco <mateusfrancovinicius@gmail.com>
2025-11-08 00:40:11 -0300

    feat: add API documentation for onboarding endpoint and enhance method annotations

--- src/main/java/br/postly/onboarding/api/controller/OnboardingController.java
+++ src/main/java/br/postly/onboarding/api/controller/OnboardingController.java
@@ -2,11 +2,21 @@ package br.postly.onboarding.api.controller;
 
 import br.postly.onboarding.api.dto.OnboardingRequest;
 import br.postly.onboarding.domain.service.OnboardingService;
+import br.postly.shared.dto.ErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/onboarding")
@@ -15,7 +25,14 @@ public class OnboardingController {
 
     private final OnboardingService onboardingService;
 
-    public ResponseEntity<Void> onboardCreator(@Valid OnboardingRequest onboardingRequest) {
+    @Operation(summary = "Onboard creator", description = "Onboard a new creator based on Onboarding Form")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Onboard registered", content = @Content()),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content()),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @PostMapping(value = "/create", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> onboardCreator(@Valid @RequestBody OnboardingRequest onboardingRequest) {
         onboardingService.onboardCreator(onboardingRequest);
         return ResponseEntity.noContent().build();
     }
